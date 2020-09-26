cfaa5f194bc31baf92c10f34bbe18287e5814eb2
Dzulfan Fadli <dzulfanfadli@gmail.com>
2020-09-26 14:05:24 +0700

    login button

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams } from "react-router";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { Spin, Space, Layout } from 'antd';
+import { Spin, Space, Layout, Button } from 'antd';
 
 import { getUserInfo } from "../../actions/auth";
 import { MENU } from '../../config';
@@ -34,12 +34,20 @@ const PageHome = () => {
 
   }, [auth_success])
 
+  const goToLoginPage = () => {
+    window.location.replace(MENU.LOGIN + window.location.origin)
+  }
+
   return (
     <Layout className="flex flex-centers">
-      <Space size="middle">
-        <Spin size="large" />
-        Initializing...
-      </Space>
+      {
+        !auth_token 
+        ? <Button type="primary" onClick={ goToLoginPage }>Login</Button>
+        : <Space size="middle">
+            <Spin size="large" />
+            Initializing...
+          </Space>
+      }
     </Layout>
   );
 };
